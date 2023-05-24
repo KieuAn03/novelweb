e771b02312f69829364dc4e621fb043fb6bb1144
Mạc An Kiều <ankieu17@gmail.com>
2023-05-24 21:57:07 +0700

    Tạo database

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(truyen)
+admin.site.register(chapter)
+admin.site.register(truyen_category)
+admin.site.register(category)
 # Register your models here.
