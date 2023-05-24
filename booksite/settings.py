c3e181bd4fd08a825706dca626e8e8847838e656
Dung3T <21521983@gm.uit.edu.vn>
2023-05-24 18:54:52 +0700

    no message

--- booksite/settings.py
+++ booksite/settings.py
@@ -75,18 +75,10 @@ WSGI_APPLICATION = 'booksite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'mssql',
-            'NAME':'KTPYTHON',
-            'USER': 'ankieu',
-            'PASSWORD': '@nki3u2003',
-            'HOST': 'kieuan03.database.windows.net',
-            'PORT': '',
-
-            'OPTIONS': {
-                'driver': 'ODBC Driver 17 for SQL Server',
-            },
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
