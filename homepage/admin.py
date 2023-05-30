f6e42ed43dab1907c3376848860978ca71155f3e
Dung3T <21521983@gm.uit.edu.vn>
2023-05-30 08:37:33 +0700

    chỉnh sửa hiển thị admin

--- homepage/admin.py
+++ homepage/admin.py
@@ -2,20 +2,45 @@ from django.contrib import admin
 from .models import *
 
 class chapterAdmin(admin.ModelAdmin):
-    list_display = ['truyen','title','id']
+    list_display = ['truyen','title','id']    
+    list_filter = ['truyen']
+    search_fields = ['truyen']
 
-class CommentInline(admin.StackedInline):
+class truyen_categoryrAdmin(admin.ModelAdmin):
+    list_display = ['Truyen','category']  
+    list_filter = ['category']
+    search_fields = ['Truyen']
+
+class CommentInline(admin.TabularInline):
     model = comment
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id','truyen','user', 'content' ,'date_published']
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['truyen','content','user', 'date_published']
+    list_filter = ['date_published']
+    search_fields = ['truyen']
 
-admin.site.register(truyen)
+class favoAdmin(admin.ModelAdmin):
+    list_display = ['truyen','user','id']
+    search_fields = ['truyen']
+    list_filter = ['truyen']
+
+class cateAdmin(admin.ModelAdmin):
+    list_display = ['title','content']
+    search_fields = ['title']
+    list_filter = ['title']
+
+class truyenmAdmin(admin.ModelAdmin):
+    list_display = ['title','author','date_published','view_count']
+    list_filter = ['author']
+
+    
+
+admin.site.register(truyen,truyenmAdmin)
 admin.site.register(chapter,chapterAdmin)
-admin.site.register(truyen_category)
-admin.site.register(category)
-admin.site.register(comment,PostAdmin)
-admin.site.register(favorate)
+admin.site.register(truyen_category,truyen_categoryrAdmin)
+admin.site.register(category,cateAdmin)
+admin.site.register(comment,CommentAdmin)
+admin.site.register(favorate,favoAdmin)
 # Register your models here.
 
 
